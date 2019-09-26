e34a8b5184736c5c18a25063e9f9971a911fd47f
Ng'ang'a Victor <ngangavictor10@gmail.com>
2019-09-26 20:58:13 +0300

    sales

---
+++ sales/edit-product.php
@@ -0,0 +1,66 @@
+<div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">EDIT PRODUCT</h5>
+            <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">×</span>
+            </button>
+          </div>
+		  <div class="form-group">
+		  <div class="form-row">
+		  <div class="col-lg-6">
+            <label for="exampleInputPassword1">Product Code</label>
+            <input class="form-control" id="exampleInputPassword1" type="text" placeholder="Product Code" required>
+          </div>
+		  <div class="col-lg-6">
+            <label for="exampleInputPassword1">Description</label>
+            <input class="form-control" id="exampleInputPassword1" type="text" placeholder="Description" required>
+          </div>
+		  <div class="col-lg-6">
+            <label for="exampleInputPassword1">Selling price</label>
+            <input class="form-control" id="exampleInputPassword1" type="text" placeholder="Selling price" required>
+          </div>
+		  <div class="col-lg-6">
+            <label for="exampleInputPassword1">Buying price</label>
+            <input class="form-control" id="exampleInputPassword1" type="text" placeholder="Buying price" required>
+          </div>
+		  <div class="col-lg-6">
+            <label for="exampleInputPassword1">Quantity</label>
+            <input class="form-control" id="exampleInputPassword1" type="text" placeholder="Quantity" required>
+          </div>
+		  <div class="col-lg-6">
+            <label for="exampleInputPassword1">Taxcode</label>
+            <select class="form-control" >
+			  <option>Select taxcode</option>
+			  <option value="">1</option>
+			  <option value="">2</option>
+			</select>
+          </div>
+		  <div class="col-lg-6">
+            <label for="exampleInputPassword1">Department</label>
+            <select class="form-control" >
+			  <option>Select Department</option>
+			  <option value="">1</option>
+			  <option value="">2</option>
+			</select>
+          </div>
+		  <div class="col-lg-6">
+            <label for="exampleInputPassword1">Image</label>
+			<input class="form-control" type="file" name="fileToUpload" id="fileToUpload">
+          </div>
+		  
+		  </div>
+		  </div>
+		  
+		  
+		  
+		  
+          
+          <div class="modal-footer">
+            <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+            <button type="submit" class="btn btn-primary" >ADD</button>
+          </div>
+        </div>
+      </div>
+    </div>
---
+++ sales/index.php
@@ -0,0 +1,101 @@
+<?php 
+	session_start();
+		$name='';$emp='';
+	if(($_SESSION['name']==$name)&&($SESSION['employeeId']==$emp))
+	{
+		header('location:../index.php');
+		exit;
+	}
+	
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <?php include"../includes/header-scripts.php";?>
+</head>
+
+<body class="fixed-nav sticky-footer bg-dark" id="page-top">
+  <!-- Navigation-->
+  <?php include"../navigation.php";?>
+  
+  <div class="content-wrapper">
+    <div class="container-fluid">
+      <!-- Breadcrumbs-->
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item">
+          <a href="#">Dashboard</a>
+        </li>
+        <li class="breadcrumb-item active">Sales</li>
+      </ol>
+      <!-- Icon Cards-->
+      
+      <!-- Area Chart Example-->
+      
+      <?php include"add-product.php";?>
+	  <?php include"edit-product.php";?>
+      <!-- Example DataTables Card-->
+      <div class="card mb-3">
+        <div class="card-header">
+          <i class="fa fa-table"></i> Sales
+		<a data-toggle="modal" data-target="#addModal" class="btn btn-primary">Calculate</a>  
+		</div>
+        <div class="card-body">
+          <div class="table-responsive">
+            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+              <thead>
+                <tr>
+				  <th>Month</th>
+				  <th>Total sales</th>
+				  <th>Year</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tfoot>
+                <tr>
+                  <th>Month</th>
+				  <th>Total sales</th>
+				  <th>Year</th>
+				  <th>Action</th>
+                </tr>
+              </tfoot>
+<?php
+$selectSales=mysqli_query($link,"SELECT * FROM tbl_sales ")or die (mysqli_error());
+while($row=mysqli_fetch_array($selectSales)){
+?>			  
+              <tbody>
+                <tr>
+                  <td><?php echo $row['month']; ?></td>
+                  <td><?php echo $row['totalSales']; ?></td>
+                  <td><?php echo $row['year']; ?></td>
+                  <td>
+				  <div class="btn-group">
+				  <a data-toggle="modal" data-target="#editModal" href="edit-product.php" class="btn btn-success">Edit</a>
+				  <button class="btn btn-danger">Delete</button>
+				  </div>
+				  </td>
+                </tr>
+                
+              </tbody>
+<?php } ?>			  
+            </table>
+          </div>
+        </div>
+        <!--<div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>-->
+      </div>
+    </div>
+    <!-- /.container-fluid-->
+    <!-- /.content-wrapper-->
+    <?php include"../includes/footer.php"; ?>
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top">
+      <i class="fa fa-angle-up"></i>
+    </a>
+    <!-- Logout Modal-->
+    <?php include"../logout-modal.php";?>
+    <?php include"../includes/footer-scripts.php"?>
+  </div>
+</body>
+
+</html>
