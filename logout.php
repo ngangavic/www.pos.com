5503afefff02405ed5f515d875deaaccee7b6b64
Ng'ang'a Victor <ngangavictor10@gmail.com>
2019-09-27 23:31:34 +0300

    logout

--- logout.php
+++ logout.php
@@ -3,6 +3,8 @@ session_start();
 //if(isset($_GET['logout']))
 //{
 	session_destroy();
+setcookie('user', "", -time() + 60 * 60 * 24 * 365, '/');
+setcookie('pass', "", -time() + 60 * 60 * 24 * 365, '/');
 	header('location:index.php?logout=true');
 	exit;
 //}
