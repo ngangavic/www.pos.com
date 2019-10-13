9e4f0fb19854862f1273023819d755491abeb3c4
Ng'ang'a Victor <ngangavictor10@gmail.com>
2019-10-13 21:56:29 +0300

    return prod

--- transaction/action.php
+++ transaction/action.php
@@ -2,6 +2,8 @@
 include("../includes/connection.php");
 
 if(isset($_POST['return'])){
+    if(isset($_POST['transactionNo'])&&isset($_POST['productCode'])&&isset($_POST['description'])&&isset($_POST['quantity'])
+        &&isset($_POST['cost'])&&isset($_POST['salesValue'])&isset($_POST['discount'])&&isset($_POST['customerCode'])){
 	$transactionNo=$_POST['transactionNo'];
 	$productCode=$_POST['productCode'];
     $description=$_POST['description'];
@@ -16,10 +18,11 @@ if(isset($_POST['return'])){
 	$pasteGroup=mysqli_query($link,"INSERT INTO tbl_return(productCode,description,transactionNo,quantity,cost,salesValue,discount,transactionDate,transactionTime,customerCode,year,month,day)VALUES('$productCode','$description','$transactionNo','$quantity','$cost','$salesValue','$discount',now(),now(),'$customerCode',now(),'$month','$day')");
 	
 	$delete=mysqli_query($link,"DELETE FROM tbl_product_his WHERE productCode='$productCode' AND transactionNo='$transactionNo'");
+header("location: return.php?message=returns&&transaction=".$transactionNo);
+    }else{
+        header("location: return.php?message=returnf");
+    }
+}else{
+    header("location: return.php?message=returnf");
 }
-
 ?>
-<script>
-alert("Item returned successsfully");
-window.location="../cashier/";
-</script>
