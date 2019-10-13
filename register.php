52efabfbe0b390baf6463b3f409c16affbaaec6d
Ng'ang'a Victor <ngangavictor10@gmail.com>
2019-10-13 10:46:58 +0300

    line format

--- register.php
+++ register.php
@@ -1,43 +1,43 @@
 <?php
 require "includes/connection.php";
-if(isset($_POST['register'])){
-    if(isset($_POST['name'])&&isset($_POST['phone'])&&isset($_POST['email'])&&isset($_POST['idno'])&&isset($_POST['password'])&&isset($_POST['password_2'])){
-    //get previous employee id
-    $stmt=$link->prepare("SELECT empId FROM tbl_employees ORDER BY empId DESC LIMIT 1 ");
-    $stmt->execute();
-    $stmt->bind_result($empId);
-    $stmt->fetch();
-    $empId=$empId+1;
-$stmt->close();
+if (isset($_POST['register'])) {
+    if (isset($_POST['name']) && isset($_POST['phone']) && isset($_POST['email']) && isset($_POST['idno']) && isset($_POST['password']) && isset($_POST['password_2'])) {
+        //get previous employee id
+        $stmt = $link->prepare("SELECT empId FROM tbl_employees ORDER BY empId DESC LIMIT 1 ");
+        $stmt->execute();
+        $stmt->bind_result($empId);
+        $stmt->fetch();
+        $empId = $empId + 1;
+        $stmt->close();
 
 //insert to database
-$name=$_POST['name'];
-$phone=$_POST['phone'];
-$email=$_POST['email'];
-$idno=$_POST['idno'];
-$password=password_hash($_POST['password'], PASSWORD_DEFAULT);
+        $name = $_POST['name'];
+        $phone = $_POST['phone'];
+        $email = $_POST['email'];
+        $idno = $_POST['idno'];
+        $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
 //check if employee already exists
-    $stmt=$link->prepare("SELECT * FROM tbl_employees WHERE email=? OR idNo=?");
-    $stmt->bind_param("ss",$email,$idno);
-    $stmt->execute();
-    $result = $stmt->get_result();
-    $count = $result->num_rows;
-if($count==0) {
-$stmt->close();
+        $stmt = $link->prepare("SELECT * FROM tbl_employees WHERE email=? OR idNo=?");
+        $stmt->bind_param("ss", $email, $idno);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $count = $result->num_rows;
+        if ($count == 0) {
+            $stmt->close();
 //insert data
-    $cat='admin';
-    $stmt = $link->prepare("INSERT INTO tbl_employees(empId,name,password,phone,email,idNo,category)VALUES(?,?,?,?,?,?,?)");
-    $stmt->bind_param("sssssss", $empId, $name, $password, $phone, $email, $idno,$cat);
-    if (!$stmt->execute()) {
-        header("location:register.php?info=error");
+            $cat = 'admin';
+            $stmt = $link->prepare("INSERT INTO tbl_employees(empId,name,password,phone,email,idNo,category)VALUES(?,?,?,?,?,?,?)");
+            $stmt->bind_param("sssssss", $empId, $name, $password, $phone, $email, $idno, $cat);
+            if (!$stmt->execute()) {
+                header("location:register.php?info=error");
+            } else {
+                header("location: register.php?info=success ");
+            }
+        } else {
+            header("location:register.php?info=error");
+        }
     } else {
-        header("location: register.php?info=success ");
-    }
-}else{
-    header("location:register.php?info=error");
-}
-}else{
         header("location:register.php?info=error");
     }
 }
@@ -46,77 +46,82 @@ $stmt->close();
 <html lang="en">
 
 <head>
-  <?php include "includes/header-scripts.php";?>
-  <script src="js/validate.js"></script>
+    <?php include "includes/header-scripts.php"; ?>
+    <script src="js/validate.js"></script>
 </head>
 
 <body class="bg-dark">
-  <div class="container">
+<div class="container">
     <div class="card card-register mx-auto mt-5">
-      <div class="card-header">Register an Account</div>
-      <div class="card-body">
-      
-      <?php  if(isset($_GET['info'])){
-        $info=$_GET['info'];
-        if($info=='error'){
-        ?>
-      <div class="alert-danger">
-      <strong>Error!</strong>Registration failed.
-      </div>
-      <?php   }else if($info=='success'){ ?>
-      <div class="alert-success">
-      <strong>Success!</strong>Registration succesful.
-      </div>
+        <div class="card-header">Register an Account</div>
+        <div class="card-body">
 
-      <?php }}?>
-        <form action="" method="post" role="form" >
-          <div class="form-group">
-            <div class="form-row">
-              <div class="col-md-6">
-                <label for="exampleInputName">Name</label>
-                <input class="form-control" name="name" type="text" placeholder="Enter first and second name" required>
-              </div>
-              <div class="col-md-6">
-                <label for="exampleInputLastName">Phone</label>
-                <input class="form-control" name="phone" type="text" placeholder="Enter phone number" required>
-              </div>
+            <?php if (isset($_GET['info'])) {
+                $info = $_GET['info'];
+                if ($info == 'error') {
+                    ?>
+                    <div class="alert-danger">
+                        <strong>Error!</strong>Registration failed.
+                    </div>
+                <?php } else if ($info == 'success') { ?>
+                    <div class="alert-success">
+                        <strong>Success!</strong>Registration succesful.
+                    </div>
+
+                <?php }
+            } ?>
+            <form action="" method="post" role="form">
+                <div class="form-group">
+                    <div class="form-row">
+                        <div class="col-md-6">
+                            <label for="exampleInputName">Name</label>
+                            <input class="form-control" name="name" type="text"
+                                   placeholder="Enter first and second name" required>
+                        </div>
+                        <div class="col-md-6">
+                            <label for="exampleInputLastName">Phone</label>
+                            <input class="form-control" name="phone" type="text" placeholder="Enter phone number"
+                                   required>
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="form-row">
+                        <div class="col-md-6">
+                            <label for="exampleInputName">Email</label>
+                            <input class="form-control" name="email" type="email" placeholder="Enter email" required>
+                        </div>
+                        <div class="col-md-6">
+                            <label for="exampleInputLastName">ID No.</label>
+                            <input class="form-control" name="idno" type="text" placeholder="Enter ID No." required>
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="form-row">
+                        <div class="col-md-6">
+                            <label for="exampleInputPassword1">Password</label>
+                            <input class="form-control" name="password" type="password" placeholder="Password"
+                                   id="pass1" required>
+                        </div>
+                        <div class="col-md-6">
+                            <label for="exampleConfirmPassword">Confirm password</label>
+                            <input class="form-control" type="password" name="password_2" placeholder="Confirm password"
+                                   id="pass2" onKeyUp="checkPass(); return false;" required>
+                        </div>
+                    </div>
+                    <span id="confirmMessage" class="alert alert-danger" style="display:none;"></span>
+                </div>
+                <button type="submit" class="btn btn-primary btn-block" name="register">Register</button>
+            </form>
+            <div class="text-center">
+                <a class="d-block small mt-3" href="index.php">Login</a>
+                <a class="d-block small" href="reset-password.php">Forgot Password?</a>
             </div>
-          </div>
-          <div class="form-group">
-            <div class="form-row">
-              <div class="col-md-6">
-                <label for="exampleInputName">Email</label>
-                <input class="form-control" name="email" type="email" placeholder="Enter email" required>
-              </div>
-              <div class="col-md-6">
-                <label for="exampleInputLastName">ID No.</label>
-                <input class="form-control" name="idno" type="text" placeholder="Enter ID No." required>
-              </div>
-            </div>
-          </div>
-          <div class="form-group">
-            <div class="form-row">
-              <div class="col-md-6">
-                <label for="exampleInputPassword1">Password</label>
-                <input class="form-control" name="password" type="password"  placeholder="Password" id="pass1" required>
-              </div>
-              <div class="col-md-6">
-                <label for="exampleConfirmPassword">Confirm password</label>
-                <input class="form-control" type="password" name="password_2"  placeholder="Confirm password" id="pass2" onKeyUp="checkPass(); return false;" required>
-              </div>
-            </div>
-			<span id="confirmMessage" class="alert alert-danger" style="display:none;"></span>
-          </div>
-          <button type="submit" class="btn btn-primary btn-block" name="register" >Register</button>
-        </form>
-        <div class="text-center">
-          <a class="d-block small mt-3" href="index.php">Login</a>
-          <a class="d-block small" href="reset-password.php">Forgot Password?</a>
         </div>
-      </div>
     </div>
-  </div>
-  <?php include "includes/footer-scripts.php"?>
+</div>
+<?php include "includes/footer-scripts.php" ?>
 </body>
 
 </html>
